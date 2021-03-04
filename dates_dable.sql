f27e30991d39ae6302ff7f85b64c5a379908c42c
aapeebles <31246169+aapeebles@users.noreply.github.com>
2021-03-04 17:17:03 -0500

    converted saved query in big query to local file

---
+++ dates_dable.sql
@@ -0,0 +1,45 @@
+DECLARE
+  min_date_var DATE;
+SET
+  min_date_var = (
+  SELECT
+    MIN(DATE(datetime)) AS min_date
+  FROM
+    `austin-animal-shelter-etl.austin_animal_shelter.raw_intakes`);
+
+
+CREATE OR REPLACE TABLE `austin-animal-shelter-etl.austin_animal_shelter.dates_table` AS
+SELECT
+  FORMAT_DATE('%F', d) AS id,
+  d AS full_date,
+  EXTRACT(YEAR
+  FROM
+    d) AS year,
+  EXTRACT(WEEK
+  FROM
+    d) AS year_week,
+  EXTRACT(DAY
+  FROM
+    d) AS year_day,
+  EXTRACT(YEAR
+  FROM
+    d) AS fiscal_year,
+  FORMAT_DATE('%Q', d) AS fiscal_qtr,
+  EXTRACT(MONTH
+  FROM
+    d) AS month,
+  FORMAT_DATE('%B', d) AS month_name,
+  FORMAT_DATE('%w', d) AS week_day,
+  FORMAT_DATE('%A', d) AS day_name,
+  (CASE
+      WHEN FORMAT_DATE('%A', d) IN ('Sunday', 'Saturday') THEN 0
+    ELSE
+    1
+  END
+    ) AS day_is_weekday,
+FROM (
+  SELECT
+    *
+  FROM
+    UNNEST(GENERATE_DATE_ARRAY(min_date_var, current_date, INTERVAL 1 DAY)) AS d )
+ORDER BY full_date desc
