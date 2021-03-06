67869d166b484d2eb919eafe4724f0031a75e5b4
aapeebles <31246169+aapeebles@users.noreply.github.com>
2021-03-05 23:52:45 -0500

    fix bq load to actually replace

--- load_data.sh
+++ load_data.sh
@@ -4,12 +4,14 @@
 # load raw_intakes into big query
 bq load --source_format=CSV \
        	--skip_leading_rows=1 \
+		--replace=true \
 	austin_animal_shelter.raw_intakes \
 	gs://shelter-etl-data-folder/raw_intakes.csv 
 
 # load raw_outcomes into big query
 bq load --source_format=CSV \
 	--skip_leading_rows=1 \
+	--replace=true \
 	austin_animal_shelter.raw_outcomes \
 	gs://shelter-etl-data-folder/raw_outcomes.csv 
 
