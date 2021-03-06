ab03e6b102a366466e8dcf4b25488090dd6cc7fd
aapeebles <31246169+aapeebles@users.noreply.github.com>
2021-03-06 14:23:40 -0500

    add parameter to not print out table head

--- run_queries.sh
+++ run_queries.sh
@@ -5,7 +5,8 @@
 bq query <./sql_scripts/dates_table.sql --use_legacy_sql=false
 
 # creates cleaned_intakes_s1
-bq query <./sql_scripts/cleaned_intakes.sql \
-    --destination_table austin_animal_shelter.cleaned_intakes_s1\
-    --replace = true \
-    --use_legacy_sql = false
+bq query <./sql_scripts/clean_intakes.sql \
+        -n=0 \
+        --destination_table austin_animal_shelter.cleaned_intakes_s1 \
+        --use_legacy_sql=false \
+        --replace=true
