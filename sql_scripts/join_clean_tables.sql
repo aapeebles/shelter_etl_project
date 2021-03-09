1406a579e7674b1159a2164178d10aebc620f5a3
aapeebles <31246169+aapeebles@users.noreply.github.com>
2021-03-09 18:57:33 -0500

    add derived variables to joined dataset

--- sql_scripts/join_clean_tables.sql
+++ sql_scripts/join_clean_tables.sql
@@ -1,7 +1,30 @@
 -- Just the starting code
-SELECT *
-FROM `austin-animal-shelter-etl.austin_animal_shelter.cleaned_intakes_s1` A
- FULL OUTER JOIN 
- `austin-animal-shelter-etl.austin_animal_shelter.cleaned_outcomes_s1` B 
- ON A.visit_id = B.visit_id
- ORDER BY A.intake_date desc 
+WITH
+  animal_join AS (
+  SELECT *
+  FROM
+    `austin-animal-shelter-etl.austin_animal_shelter.cleaned_intakes_s1` A
+  FULL OUTER JOIN 
+    
+      `austin-animal-shelter-etl.austin_animal_shelter.cleaned_outcomes_s1` B
+  ON
+    A.visit_id = B.visit_id
+  ORDER BY
+    A.intake_date DESC,
+    A.intake_time DESC)
+SELECT
+  *,
+IF
+  (outcome_date IS NOT NULL,
+    DATE_DIFF(outcome_date,intake_date, DAY),
+    NULL) AS days_in_shelter,
+IF
+  (reproductive_status_outcome IS NOT NULL, 
+  IF
+    (reproductive_status_intake != reproductive_status_outcome, 1,0), 
+    NULL) as neutered_during_stay
+FROM
+  animal_join
+ORDER BY
+  intake_date DESC,
+  intake_time DESC
