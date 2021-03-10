09a835d605f144afd5026723fcd791fabebc154f
aapeebles <31246169+aapeebles@users.noreply.github.com>
2021-03-09 19:28:33 -0500

    begin current in shelter sql

---
+++ sql_scripts/in_shelter_now.sql
@@ -0,0 +1,9 @@
+select *,
+date_diff(intake_date, birth_date, YEAR) as age_years,
+date_diff(intake_date, birth_date, MONTH) as age_months,
+date_diff(intake_date, birth_date, DAY) as age_days
+from `austin-animal-shelter-etl.austin_animal_shelter.joined_intakes_outcomes_s2`
+where outcome_date IS NULL AND intake_date IS NOT NULL
+ORDER BY
+  intake_date DESC,
+  intake_time DESC
