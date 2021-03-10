4ee5e5821c60d096e4fb52f2a7eeecfbdf89dc3a
aapeebles <31246169+aapeebles@users.noreply.github.com>
2021-03-09 19:09:40 -0500

    remove duplicate columns

--- sql_scripts/join_clean_tables.sql
+++ sql_scripts/join_clean_tables.sql
@@ -1,7 +1,11 @@
--- Just the starting code
 WITH
   animal_join AS (
-  SELECT *
+    SELECT A.*,
+    B.outcome_date,
+    B.outcome_time,
+    B.outcome_type,
+    B.outcome_subtype,
+    B.reproductive_status_outcome
   FROM
     `austin-animal-shelter-etl.austin_animal_shelter.cleaned_intakes_s1` A
   FULL OUTER JOIN 
@@ -18,11 +22,7 @@ IF
   (outcome_date IS NOT NULL,
     DATE_DIFF(outcome_date,intake_date, DAY),
     NULL) AS days_in_shelter,
-IF
-  (reproductive_status_outcome IS NOT NULL, 
-  IF
-    (reproductive_status_intake != reproductive_status_outcome, 1,0), 
-    NULL) as neutered_during_stay
+IF(reproductive_status_outcome IS NOT NULL, IF(reproductive_status_intake != reproductive_status_outcome, 1,0), NULL) as neutered_during_stay
 FROM
   animal_join
 ORDER BY
