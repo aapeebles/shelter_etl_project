9f029f4f60ea7701a0874601c7092904b909918f
aapeebles <31246169+aapeebles@users.noreply.github.com>
2021-03-11 16:32:13 -0500

    drop unecessary columns from in shelter now table

--- sql_scripts/in_shelter_now.sql
+++ sql_scripts/in_shelter_now.sql
@@ -1,7 +1,19 @@
-select *,
-date_diff(intake_date, birth_date, YEAR) as age_years,
-date_diff(intake_date, birth_date, MONTH) as age_months,
-date_diff(intake_date, birth_date, DAY) as age_days
+select visit_id,
+        animal_id,
+        name,
+        animal_type,
+        intake_date,
+        intake_time,
+        intake_type,
+        intake_condition,
+        needs_attention,
+        sex,
+        reproductive_status_intake,
+        birth_date,
+        breed,        
+        date_diff(intake_date, birth_date, YEAR) as age_years,
+        date_diff(intake_date, birth_date, MONTH) as age_months,
+        date_diff(intake_date, birth_date, DAY) as age_days
 from `austin-animal-shelter-etl.austin_animal_shelter.joined_intakes_outcomes_s2`
 where outcome_date IS NULL AND intake_date IS NOT NULL
 ORDER BY
