aea7cfcb40decfbed5e4b60ae9081a2417fb14cb
aapeebles <31246169+aapeebles@users.noreply.github.com>
2021-03-14 18:05:30 -0400

    reorg files

--- etl_scripts/load_data.sh
+++ etl_scripts/load_data.sh
@@ -16,5 +16,5 @@ bq load --source_format=CSV \
 	gs://shelter-etl-data-folder/raw_outcomes.csv 
 
 # creates dates table
-bq query <./sql_scripts/dates_table.sql --use_legacy_sql=false
+bq query <~/bin/sql_scripts/dates_table.sql --use_legacy_sql=false --replace=true 
 
--- etl_scripts/main_etl_flow.sh
+++ etl_scripts/main_etl_flow.sh
@@ -2,42 +2,42 @@
 # this script will run the bulk of my etl
 
 # creates cleaned_intakes_s1
-bq query <./sql_scripts/clean_intakes.sql \
+bq query <../sql_scripts/clean_intakes.sql \
         -n=0 \
         --destination_table austin_animal_shelter.cleaned_intakes_s1 \
         --use_legacy_sql=false \
         --replace=true
 
 # creates cleaned_outcomes_s1
-bq query <./sql_scripts/clean_outcomes.sql \
+bq query <../sql_scripts/clean_outcomes.sql \
         -n=0 \
         --destination_table austin_animal_shelter.cleaned_outcomes_s1 \
         --use_legacy_sql=false \
         --replace=true
 
 # creates joined_intakes_outcomes_s2
-bq query <./sql_scripts/join_clean_tables.sql \
+bq query <../sql_scripts/join_clean_tables.sql \
         -n=0 \
         --destination_table austin_animal_shelter.joined_intakes_outcomes_s2 \
         --use_legacy_sql=false \
         --replace=true
 
 # creates animal_history_records_s3
-bq query <./sql_scripts/animal_visit_history.sql \
+bq query <../sql_scripts/animal_visit_history.sql \
         -n=0 \
         --destination_table austin_animal_shelter.animal_history_records_s3 \
         --use_legacy_sql=false \
         --replace=true
 
 # creates animal_history_records_s3
-bq query <./sql_scripts/in_shelter_now.sql \
+bq query <../sql_scripts/in_shelter_now.sql \
         -n=0 \
         --destination_table austin_animal_shelter.animals_without_outcomes_s3 \
         --use_legacy_sql=false \
         --replace=true
 
 # creates intakes_outcomes_s3
-bq query <./sql_scripts/cleaned_intakes_outcomes.sql \
+bq query <../sql_scripts/cleaned_intakes_outcomes.sql \
         -n=0 \
         --destination_table austin_animal_shelter.intakes_outcomes_s3 \
         --use_legacy_sql=false \
